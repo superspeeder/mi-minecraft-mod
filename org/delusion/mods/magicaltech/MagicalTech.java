38df451e48dee178e489e71745449b65f42f5132
superspeeder <andy@delusion.org>
2019-05-03 12:17:12 -0700

    Create MagicalTech.java

---
+++ org/delusion/mods/magicaltech/MagicalTech.java
@@ -0,0 +1,15 @@
+package org.delusion.mods.magicaltech;
+
+import org.delusion.mods.delusionallib.IMainMod;
+import cofh.redstoneflux.RedstoneFluxProps;
+
+// ill put in the imports when I use my IDE
+
+
+@Mod(modid=MagicalTech.modid, name=MagicalTech.name, version=MagicalTech.version)
+public class MagicalTech implements IMainMod {
+  public static final String modid = "magicaltech";
+  public static final String modid = "Magical Tech";
+  public static final String modid = "0.0.1";
+  public static final String dependencies = "required-after:delusionallib";
+}
