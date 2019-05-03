cf9cf2c438c4fcc68cfda2bd6461feef68935e91
superspeeder <andy@delusion.org>
2019-05-03 12:33:49 -0700

    Update MagicalTech.java

--- org/delusion/mods/magicaltech/MagicalTech.java
+++ org/delusion/mods/magicaltech/MagicalTech.java
@@ -1,6 +1,7 @@
 package org.delusion.mods.magicaltech;
 
 import org.delusion.mods.delusionallib.IMainMod;
+import org.delusion.mods.delusionallib.BaseModificationRegistrationHandler;
 import cofh.redstoneflux.RedstoneFluxProps;
 
 // ill put in the imports when I use my IDE
@@ -12,4 +13,25 @@ public class MagicalTech implements IMainMod {
   public static final String modid = "Magical Tech";
   public static final String modid = "0.0.1";
   public static final String dependencies = "required-after:delusionallib";
+  
+  @EventHandler
+  public static void preInit(FMLPreInitializationEvent event) {
+    RegistrationHandler.preRegistrationInit();
+  }
+  
+  @EventHandler
+  public static void init(FMLInitializationEvent event) {
+  }
+  
+  @EventHandler
+  public static void postInit(FMLPostInitializationEvent event) {
+  }
+  
+  @EventBusSubscriber
+  public static class RegistrationHandler extends BaseModificationRegistrationHandler {
+    @Override
+    public static void preRegistrationInit() {
+      //addItemRegister(ModItems.class)
+    }
+  }
 }
